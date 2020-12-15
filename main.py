516c712a19e1a586b2bbe03fa35e751baddd46d6
Brock Sponenburgh <bsponenburgh9815@gmail.com>
2020-12-15 13:11:02 -0700

    Made it better

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ import random
 
 file = open("ChristmasPictures")
 wordList = []
+numList = []
+go = True
 
 for words in file:
     wordList.append(words)
 
-num = random.randint(0, len(wordList))
-print(wordList[num])
+while go:
+    answer = input("Keep going? Y/N ").lower()
+
+    if answer == "y":
+        go = True
+    else:
+        go = False
+        break
+    
+    num = random.randint(0, len(wordList))
+    
+    if num in numList and answer != "y":
+        continue
+    else:
+        numList.append(num)
+        print(wordList[num])
+
+print("Thanks for playing!")
+
 file.close()
